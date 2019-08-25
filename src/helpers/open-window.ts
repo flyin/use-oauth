f3f7167129019eba3a2df7101143d716a91a6eda
flyin <alex@alex-web.ru>
2019-08-25 13:54:15 +0300

    Complete lib

---
+++ src/helpers/open-window.ts
@@ -0,0 +1,40 @@
+export function openWindow(url: string, title: string, w: number, h: number): Window | null {
+    const userAgent = navigator.userAgent,
+        isMobile = (): boolean =>
+            /\b(iPhone|iP[ao]d)/.test(userAgent) ||
+            /\b(iP[ao]d)/.test(userAgent) ||
+            /Android/i.test(userAgent) ||
+            /Mobile/i.test(userAgent);
+
+    const screenX = typeof window.screenX !== 'undefined' ? window.screenX : window.screenLeft;
+    const screenY = typeof window.screenY !== 'undefined' ? window.screenY : window.screenTop;
+    const outerWidth = typeof window.outerWidth !== 'undefined' ? window.outerWidth : document.documentElement.clientWidth;
+    const outerHeight = typeof window.outerHeight !== 'undefined' ? window.outerHeight : document.documentElement.clientHeight - 22;
+    const targetWidth = isMobile() ? 0 : w;
+    const targetHeight = isMobile() ? 0 : h;
+    const v = screenX < 0 ? window.screen.width + screenX : screenX;
+    const left = v + (outerWidth - targetWidth) / 2;
+    const right = screenY + (outerHeight - targetHeight) / 2.5;
+
+    const features = [];
+
+    if (targetWidth !== null) {
+        features.push('width=' + targetWidth);
+    }
+
+    if (targetHeight !== null) {
+        features.push('height=' + targetHeight);
+    }
+
+    features.push('left=' + left);
+    features.push('top=' + right);
+    features.push('scrollbars=1');
+
+    const newWindow = window.open(url, title, features.join(','));
+
+    if (newWindow && newWindow.focus) {
+        newWindow.focus();
+    }
+
+    return newWindow;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,135 @@
-import { MutableRefObject, useCallback, useRef } from 'react';
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
+import { openWindow } from './helpers';
 
-interface Hook {
-    focus: () => void;
+interface State {
+    isCancelled: boolean;
+    isFatal: boolean;
+    isLoading: boolean;
+    isLoaded: boolean;
+    isSuccessful: boolean;
+    provider: string;
+    message?: string;
 }
 
-export function useOAuth(): Hook {
-    const openedWindow: MutableRefObject<Window | null> = useRef(null);
+interface Hook {
+    auth: (provider: string) => void;
+    focus: () => void;
+    state: State;
+}
 
-    const focus = useCallback((): void => {
+type ProviderURL = ((provider: string) => string) | string;
+
+interface HookParams {
+    onSuccess?: (state: State) => void;
+    providerURL: ProviderURL;
+}
+
+const initialState: State = {
+    isCancelled: false,
+    isFatal: false,
+    isLoading: false,
+    isLoaded: false,
+    isSuccessful: false,
+    provider: '',
+    message: '',
+};
+
+function getProviderURL(providerURL: ProviderURL, provider: string): string {
+    if (typeof providerURL === 'function') {
+        return providerURL(provider);
+    }
+
+    const newURL = providerURL.replace(/\/$/, '');
+    return `${newURL}/${provider}/`;
+}
+
+function start(provider: string): State {
+    return {
+        ...initialState,
+        provider: provider,
+    };
+}
+
+function cancel(provider: string): State {
+    return {
+        ...initialState,
+        isFatal: true,
+        isLoading: false,
+        isLoaded: false,
+        isCancelled: true,
+        provider: provider,
+    };
+}
+
+function error(provider: string, message: string): State {
+    return {
+        ...initialState,
+        message: message,
+        provider: provider,
+        isLoading: false,
+        isLoaded: true,
+        isFatal: true,
+    };
+}
+
+export function useOAuth({ onSuccess, providerURL }: HookParams): Hook {
+    const openedWindow: MutableRefObject<Window | null> = useRef(null);
+    const [state, setState] = useState(initialState);
+
+    // check window state
+    useEffect(() => {
+        let interval: number;
+
+        if (state.isLoading) {
+            interval = window.setInterval(() => {
+                if (!openedWindow.current || !openedWindow.current.closed) return;
+
+                clearInterval(interval);
+
+                if (state.isLoading) {
+                    setState(cancel(state.provider));
+                }
+            }, 150);
+        }
+
+        return (): void => clearInterval(interval);
+    }, [state.isLoading, state.provider]);
+
+    const auth = useCallback((provider: string): void => {
+        setState(start(provider));
+        openedWindow.current = openWindow(getProviderURL(providerURL, provider), 'Auth', 660, 370);
+
+        if (!openedWindow.current) {
+            setState(error(provider, "Can't open window"));
+        }
+    }, []);
+
+    const focus = useCallback(() => {
         if (openedWindow.current) {
             openedWindow.current.focus();
         }
     }, [openedWindow]);
 
-    return { focus };
+    const windowMessageListener = useCallback(
+        event => {
+            if (event.data.source !== 'auth') {
+                return;
+            }
+
+            const nextState = { ...state, ...event.data };
+            setState(nextState);
+
+            if (typeof onSuccess === 'function') {
+                onSuccess(nextState);
+            }
+        },
+        [state],
+    );
+
+    useEffect(() => {
+        window.addEventListener('message', windowMessageListener);
+        return (): void => window.removeEventListener('message', windowMessageListener);
+    }, [windowMessageListener]);
+
+    return { auth, focus, state };
 }
