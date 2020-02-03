4a39eec40993f93d69705b0c4130c346268b469d
flyin <alex@alex-web.ru>
2020-02-03 20:47:25 +0300

    Fix test runner

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,10 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   setupFilesAfterEnv: ['<rootDir>/node_modules/jest-enzyme/lib/index.js'],
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/', '<rootDir>/public/'],
   testEnvironment: 'jsdom',
   transform: {
     '.*': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest',
   },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
 };
--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,3 +1,3 @@
 test('create a new hello', (): void => {
-    expect(true).toBe(true);
+  expect(true).toBe(true);
 });
