000a6fe42f38ce27146f9edf4061300fe5d3af38
flyin <dev@polesov.com>
2023-12-18 17:47:22 +0300

    AI generated changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { useCallback, useMemo, useReducer, useRef } from 'react'
 import { openWindow } from './helpers'
 
+// Type definition for OAuth state
 export type OAuthState = {
   isLoading: boolean
   isCancelled: boolean
@@ -9,13 +10,16 @@ export type OAuthState = {
   payload?: unknown
 }
 
+// Type definition for available action types
 type ActionType = 'start' | 'cancel' | 'complete' | 'error'
 
+// Type definition for an action object
 type Action = {
   type: ActionType
   payload?: unknown
 }
 
+// Initial state for OAuth
 const initialState: OAuthState = {
   isLoading: false,
   isCancelled: false,
@@ -23,6 +27,7 @@ const initialState: OAuthState = {
   isError: false,
 }
 
+// Options type for the OAuth window
 type Options = {
   window?: {
     height: number
@@ -30,41 +35,50 @@ type Options = {
   }
 }
 
+// Reducer function for the OAuth process
 const reducer = (state: OAuthState, action: Action): OAuthState => {
+  // Handle actions and return the updated state
   switch (action.type) {
     case 'start': {
+      // Start the loading process
       return { ...initialState, isLoading: true }
     }
     case 'complete': {
+      // Mark the OAuth process as completed
       return { ...state, isCompleted: true, isLoading: false, payload: action.payload }
     }
     case 'cancel': {
+      // Mark the OAuth process as cancelled
       return { ...state, isCancelled: true, isLoading: false }
     }
     case 'error': {
+      // Mark the OAuth process as resulted in an error
       return { ...state, isError: true, isLoading: false, payload: action.payload }
     }
   }
 }
 
+// Custom hook for OAuth process
 export function useOAuth() {
-  const openedWindow = useRef<Window | null>(null)
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const openedWindow = useRef<Window | null>(null)  // Reference to the opened window
+  const [state, dispatch] = useReducer(reducer, initialState)  // OAuth state and dispatcher
 
+  // Function to start the OAuth process
   const start = useCallback(
     (providerURL: string, options: Options): Promise<unknown> =>
       new Promise((resolve, reject) => {
-        dispatch({ type: 'start' })
+        dispatch({ type: 'start' })  // Start the OAuth process
 
+        // Use helper function to open a new window for authentication
         openedWindow.current = openWindow(
           providerURL,
           'Auth',
-
-          options.window?.width ?? 660,
-          options.window?.height ?? 370,
+          options.window?.width ?? 660, // Default window width
+          options.window?.height ?? 370, // Default window height
         )
 
         if (!openedWindow.current) {
+          // If window fails to open, dispatch an error and reject the promise
           const payload = { message: "Can't open window" }
           dispatch({ type: 'error', payload })
           return reject(payload)
@@ -72,24 +86,30 @@ export function useOAuth() {
 
         let isDone = false
 
+        // Listener for receiving the message from the authentication window
         const messageListener = (event: MessageEvent): void => {
+          // Ensure message is from the expected source
           if (event.data.source !== 'oauth') {
             return
           }
 
+          // Cleanup listener and mark as done
           window.removeEventListener('message', messageListener)
           isDone = true
           dispatch({ type: 'complete' })
           resolve(event.data.payload)
         }
 
+        // Register listener for the message from the authentication window
         window.addEventListener('message', messageListener)
 
+        // Poll to check if the window has been closed without sending a message
         const intervalId = window.setInterval(() => {
           if (openedWindow.current && !openedWindow.current.closed) return
           clearInterval(intervalId)
 
           if (!isDone) {
+            // If window is closed and no message was received, dispatch a cancel and reject the promise
             const payload = { message: "Can't open window" }
             dispatch({ type: 'cancel', payload })
             reject(payload)
@@ -99,11 +119,14 @@ export function useOAuth() {
     [],
   )
 
+  // Function to focus the opened OAuth window
   const focus = useCallback(() => {
     if (openedWindow.current && !openedWindow.current.closed) {
       openedWindow.current.focus()
     }
   }, [])
 
+  // useMemo to memoize the values and functions to prevent unnecessary renders
   return useMemo(() => ({ start, focus, state }), [focus, start, state])
 }
+
